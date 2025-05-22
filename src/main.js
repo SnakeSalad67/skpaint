8c281913390c0182ca32866ca5cbbe948f1e1302
TacoFetish <joelkoll628@gmail.com>
2025-05-21 21:02:42 -0400

    undo error

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./public/vite.svg" className="logo react" alt="React logo" />
-
-        <h1>Hello from Amplify</h1>
-      </header>
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
     </div>
-  );
-}
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-export default App;
+setupCounter(document.querySelector('#counter'))

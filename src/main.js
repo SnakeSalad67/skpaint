799ff1ab3d71f9022b0574fd1534a4c71ffe5ae5
TacoFetish <joelkoll628@gmail.com>
2025-05-21 20:54:33 -0400

    changes for amplify

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./public/vite.svg" className="logo react" alt="React logo" />
+
+        <h1>Hello from Amplify</h1>
+      </header>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  );
+}
 
-setupCounter(document.querySelector('#counter'))
+export default App;
